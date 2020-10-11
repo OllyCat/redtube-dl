9799b345784113f8ebfc2a2da50ecb898c115926
Oleg Adianov <oleg.adianov@gmail.com>
2020-10-11 12:01:18 +0300

    Refactoring: - http -> fasthttp - защита от скрипта "Loading..."

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
+	"math/rand"
 	"net/url"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/OllyCat/ariago"
+	"github.com/valyala/fasthttp"
 )
+
+var (
+	r      *regexp.Regexp
+	fr     *regexp.Regexp
+	client *fasthttp.Client
+)
+
+func init() {
+	r = regexp.MustCompile(`meta name="twitter:player" content="(https://embed.redtube.com/\?id=\d+)"`)
+	fr = regexp.MustCompile(`flashvars_vid\d* = ({.*});`)
+	client = &fasthttp.Client{}
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
 	var wg sync.WaitGroup
@@ -47,22 +61,15 @@ func parce(u string) (string, error) {
 		return "", errors.New("Invalid URL")
 	}
 
-	resp, err := http.Get(u)
-	if err != nil {
-		return "", errors.New(fmt.Sprintf("Could not get url: %v\n", err))
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
+	_, body, err := client.Get(nil, u)
 	if err != nil {
 		return "", errors.New(fmt.Sprintf("Could not get url: %v\n", err))
 	}
 
-	r := regexp.MustCompile(`meta name="twitter:player" content="(https://embed.redtube.com/\?id=\d+)"`)
 	res := r.FindSubmatch(body)
 
 	if len(res) < 2 {
-		return "", errors.New(fmt.Sprintf("Could not find player url: %v\n", err))
+		return "", fmt.Errorf("Could not find player url: %w\n", err)
 	}
 
 	return string(res[1]), nil
@@ -71,25 +78,30 @@ func parce(u string) (string, error) {
 func get(id string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	res, err := http.Get(id)
-	if err != nil {
-		log.Printf("ERROR: id %s get error\n", id)
-		return
-	}
-	defer res.Body.Close()
+	var fres [][]byte
 
-	fr := regexp.MustCompile(`flashvars_vid\d* = ({.*});`)
+	// 10 повторов для защиты от скрипта "Loading..."
+	var i int
+	for {
+		_, body, err := client.Get(nil, id)
+		if err != nil {
+			log.Printf("ERROR: id %s get error\n", id)
+			return
+		}
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Printf("ERROR: id %s body get error\n", id)
-		return
-	}
-
-	fres := fr.FindSubmatch(body)
-	if len(fres) < 2 {
-		log.Printf("ERROR: id %s flashvars not found\n", id)
-		return
+		// если патерн найдет - прерываем цыкл
+		fres = fr.FindSubmatch(body)
+		if len(fres) > 1 {
+			break
+		}
+		// если нет - случайная задержка и повтор
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+		i++
+		// если повторов было больше 10, то прерываемся с ошибкой
+		if i >= 10 {
+			log.Printf("ERROR: id %s flashvars not found.\n", id)
+			return
+		}
 	}
 
 	js := JsonScript{}
